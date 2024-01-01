5253ca059b3e57e6fdf68c30d1a483ac13a46386
ToanNguyen3007 <toannhqt2003@gmail.com>
2024-01-01 23:33:39 +0700

    reactfirst

---
+++ src/components/DateContaier.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import DayDisplay from "./DayDisplay";
+import "./date.css"
+const DateContainer = () => {
+    const [date,setDate] = useState(Date.now()); //useState
+    return (  
+        <>
+            <input type="date" className="date-input" onChange={(e)=> setDate(e.target.value)}/>
+            <DayDisplay date={date}/> 
+        </>
+    );
+}
+ 
+export default DateContainer;
